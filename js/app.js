d296286270c7f8d51f6a37bdd971167abc6673d8
Justyna Bielecka <justynbielecka@gmail.com>
2016-06-24 12:56:56 +0200

    small changes in class names to follow BEM metodology and changes in app.js - slider athlets__list__itmes

--- js/app.js
+++ js/app.js
@@ -1,3 +1,47 @@
 $(function(){
 
+  var athletsListItems = document.querySelectorAll(".athlets__list__item"); //array of all athlets__list__item
+  var counterAthlets = 0; //variable to indicate the athlets__list__item which is active
+  var athletsIndicator = document.querySelector(".athlets__indicator").children; //array of athlets__indicator
+  var smallesWidth = false; //variable which is true when the widh of window is less than 440px
+
+//function to check if the max-width is less than 440px
+  function testMatch(){
+    var mW = window.matchMedia("(max-width: 440px)");
+    mW.addListener(widthChange);
+    widthChange(mW);
+  }
+
+  function widthChange(mW){
+      if(mW.matches){
+        smallesWidth = true;
+      }else{
+        smallesWidth = false;
+      }
+  }
+
+  testMatch();
+
+//function which give event on click to athlets__list__item 
+  function changeAthletsInfo(){
+    for(var i=0; i<athletsListItems.length; i++){
+        athletsListItems[i].addEventListener("click", function(event){
+          console.log(smallesWidth)
+          if(smallesWidth === true){
+            console.log("jestem" + counterAthlets);
+
+            athletsListItems[counterAthlets].classList.remove("list-item--active");
+            athletsIndicator[counterAthlets].classList.remove("active");
+            if(counterAthlets === 2){
+              counterAthlets = -1;
+            }
+            counterAthlets++
+            athletsListItems[counterAthlets].classList.add("list-item--active");
+            athletsIndicator[counterAthlets].classList.add("active");
+          }
+        });
+      }
+  };
+changeAthletsInfo();
+
 });
